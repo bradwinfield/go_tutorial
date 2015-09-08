d9775a8e3cf8ef254b36e6fb95e2e7019b50ef20
bradwinfield <brad.winfield@maryville.com>
2015-09-08 09:30:52 -0500

    Version 1.3

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Printf("This is a Go program. It simply prints out \"hello, world.\".\n")
-	fmt.Printf("Version 1.2.\n")
+	fmt.Printf("Version 1.3.\n")
 	fmt.Printf("hello, world.\n")
 }
